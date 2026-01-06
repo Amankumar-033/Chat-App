844a662c28ce7bdce7800e67245e241eba86b2c9
Amankumar-033 <amanbkp9135@gmail.com>
2026-01-06 14:48:24 +0530

    auto-commit

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,13 +1,19 @@
 import React from 'react'
 import { BiSearchAlt2 } from "react-icons/bi";
+import OtherUsers from './OtherUsers';
 
 const Sidebar = () => {
   return (
-    <div>
+    <div className='border-r border-slate-500 flex flex-col p-4'>
       <form action="" className='flex items-center gap-2'>
         <input className='input input-bordered rounded-md' type="text"  placeholder='Search...'/>
         <button type='submit' className='btn bg-zinc-500 text-white'> <BiSearchAlt2 className='w-6 h-6 outline-none'/> </button>
-        </form>
+    </form>
+    <div className='divider px-3'></div>
+    <OtherUsers/>
+    <div className='mt-2'>
+        <button className='btn btn-sm'> Logout </button>
+    </div>
     </div>
   )
 }
