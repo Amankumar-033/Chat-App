84a23241e9941942c5d706415d0583a77612e656
Amankumar-033 <amanbkp9135@gmail.com>
2026-01-08 04:22:22 +0530

    auto-commit

--- backEnd/index.js
+++ backEnd/index.js
@@ -7,7 +7,9 @@ import messageRouter from "./routes/messageRoute.js"
 import cookieParser from "cookie-parser";
 import { app, server } from "./socket/socket.js";
 dotenv.config({});
+import path from "path";
 
+const __dirname = path.resolve();
 
 //Middleware for parsing json string to req.body....
 app.use(express.json());
@@ -26,6 +28,16 @@ const PORT = process.env.PORT || 5000;
 //Routes...
 app.use("/api/user", userRouter);
 app.use("/api/message", messageRouter);
+
+if (process.env.NODE_ENV === "production") {
+    // Backend frontend ke dist folder ko serve karega
+    app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+    // Kisi bhi route par React ka index.html bhejega
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+    });
+}
 
 
 server.listen(PORT, () => {
