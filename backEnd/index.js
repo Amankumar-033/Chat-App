f46cca808c2f51e15a29ef27333f44455ea2bd70
Amankumar-033 <amanbkp9135@gmail.com>
2026-01-08 18:44:19 +0530

    Add file

--- backEnd/index.js
+++ backEnd/index.js
@@ -33,11 +33,10 @@ app.use("/api/message", messageRouter);
 
 
 if(process.env.NODE_ENV === "production") {
-    // Ye line root/frontend/dist ko serve karegi
-    app.use(express.static(path.join(__dirname, "/frontend/dist")));
+    app.use(express.static(path.join(__dirname, "/frontend/build")));
 
     app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+        res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
     });
 };
 
